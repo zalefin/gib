26d2da17fafcad5fca554320068c0a873434125a
Zach Lefin <zachlefin@gmail.com>
2020-07-21 20:25:25 -0400

    add arg repeat, error handle

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <limits.h>
+#include <errno.h>
 #include "morpheme.h"
 
 
@@ -11,12 +13,25 @@ int main(int argc, char **argv){
     clock_gettime(CLOCK_MONOTONIC, &ts);
     srand((time_t) ts.tv_nsec);
 
-    printf("%s%s%s%s%s\n",
-	    get_conson(),
-	    get_vowel(),
-	    get_conson(),
-	    get_vowel(),
-	    get_conson());
+    int n = 1;
+    if(argc > 1){
+	char* p;
+	errno = 0; // not 'int errno', because the '#include' already defined it
+	long arg = strtol(argv[1], &p, 10);
+	if (*p != '\0' || errno != 0) {
+	    return 1; // In main(), returning non-zero means failure
+	}
+	n = arg;
+    }
 
+    for(int i = 0; i < n; i++){
+	printf("%s%s%s%s%s ",
+		get_conson(),
+		get_vowel(),
+		get_conson(),
+		get_vowel(),
+		get_conson());
+    }
+    printf("\n");
     return 0;
 }
